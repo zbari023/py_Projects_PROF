f16e94c5bd037e14a6cf7f927f7b63e1ce6af6c0
zbari023 <barry450643@gmail.com>
2023-03-29 23:08:17 +0200

    new function

--- New_Game.py
+++ New_Game.py
@@ -10,3 +10,12 @@ def no_repait(sentence):
     print(' '.join(new_Out))      # list to str
 
 no_repait('my name is is is Bari')  # my name is Bari
+
+# new function that give us how many letter in the one element
+def w_count(names):
+    new_out2=[]
+    for i in names:
+        new_out2.append(len(i))
+
+    print(new_out2)
+w_count(['zaid' ,'Bari' ,'Ibrahim'])     # [4, 4, 7]
